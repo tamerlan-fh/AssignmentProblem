3100841ff9e764da50fae469b9f73c353eae61f4
Tamerlan <tamerlan-fh@ya.ru>
2017-12-12 10:44:11 +0500

    start

---
+++ AssignmentProblem/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentProblem
+{
+    class Repository
+    {
+        public static Repository Instance
+        {
+            get
+            {
+                if(instance == null)
+                    instance = new Repository();
+                return instance;
+            }
+        }
+        private static Repository instance;
+        private Repository() { }
+    }
+}
---
+++ AssignmentProblem/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentProblem.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
